b02c1518f06e4f9a4145d35308557ae15b323982
Chris <christophercrowd43@gmail.com>
2021-02-26 09:14:56 -0500

    refactor: Cleaned up select code, added default, disabled field

--- src/pages/login.js
+++ src/pages/login.js
@@ -7,17 +7,24 @@ const Login = () => {
   const dispatch = useDispatch();
 
   const { users } = useSelector((state) => state.users);
-
-  console.log(users);
+  const [loggedUser, setloggedUser] = useState({});
 
   useEffect(() => {
     dispatch(fetchAllusers());
   }, [dispatch]);
 
+  const userHandler = (e) => {
+    console.log(e.target.value);
+    setloggedUser(e.target.value);
+  };
+
   return (
     <>
       <h1>Login</h1>
-      <select>
+      <select value="login" onChange={(e) => userHandler(e)}>
+        <option value="login" disabled>
+          Login Please
+        </option>
         {users &&
           Object.keys(users).map((key) => (
             <option key={users[key].id} value={users[key].id}>
