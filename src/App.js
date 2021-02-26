7adbd79ca736658428f93a3ac78248a74b879045
Chris <christophercrowd43@gmail.com>
2021-02-26 11:28:11 -0500

    refactor:Updated route order and paths

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/login" component={Login} />
+        <Route path="/" exact component={Login} />
+        <Route path="/home" component={Home} />
         <Route path="/questions" component={Question} />
         <Route path="/leaders" component={Leaders} />
       </Switch>
